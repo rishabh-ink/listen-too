a7b6e3a16ad5771b9e855463ae7e6a1b2ba0a16c
Rishabh Rao <rishabhsrao@yahoo.com>
2012-07-14 16:04:18 +0530

    Only models have ko.observable.

--- js/modules/model/Artist.js
+++ js/modules/model/Artist.js
@@ -1,21 +1,19 @@
 "use strict";
 
 define(["jquery", "use!debug", "knockoutjs"], function(jQuery, debug, ko) {
-	var content = {
-		name: ko.observable("Taylor Swift")
-	};
+	var name = ko.observable("Taylor Swift");
 
 	var toString = function() {
 		return {
 			classname: "model.Artist",
 			members: {
-				content: content
+			name: name
 			}
 		}
 	}
 
 	return {
 		toString: toString,
-		content: content
+		name: name
 	}
 });
